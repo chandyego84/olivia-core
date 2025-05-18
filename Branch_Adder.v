ae9d76eb6304523552e3bc1c5ac5050fdbd74c0e
Chandler Juego <chandlerjuego101@gmail.com>
2025-05-18 16:51:59 -0700

    more commenting

--- Branch_Adder.v
+++ Branch_Adder.v
@@ -1,6 +1,6 @@
 module Branch_Adder(
     input wire [63:0] pc_out,
-    input wire signed [63:0] branch_off, // relative byte-offset
+    input wire signed [63:0] branch_off, // relative byte-offset (e.g., B #2 == 8 byte-offset from PC)
     output wire [63:0] result
 );
 
